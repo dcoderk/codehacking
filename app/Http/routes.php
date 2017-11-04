8557cc6436167f75969e80caa21ad9f71a07143f
dcoderk <webmarket2k@gmail.com>
2017-11-04 18:58:57 +0800

    asset added and admin master page setup

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -18,3 +18,14 @@ Route::get('/', function () {
 Route::auth();
 
 Route::get('/home', 'HomeController@index');
+
+
+Route::get('/admin', function(){
+
+	return view('admin.index');
+
+});
+
+
+Route::resource('admin/users', 'AdminUsersController');
+
