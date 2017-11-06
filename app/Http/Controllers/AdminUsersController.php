8a8d06092f28f73db6de918ceda4b3cb4182cfb4
dcoderk <webmarket2k@gmail.com>
2017-11-06 15:43:52 +0800

    laravel/collective html package added

--- app/Http/Controllers/AdminUsersController.php
+++ app/Http/Controllers/AdminUsersController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+
+use App\User;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
@@ -16,7 +18,10 @@ class AdminUsersController extends Controller
     public function index()
     {
         
-        return view('admin.users.index');
+        $users = User::all();
+
+
+        return view('admin.users.index', compact('users'));
 
     }
 
--- resources/views/admin/users/create.blade.php
+++ resources/views/admin/users/create.blade.php
@@ -1,1 +1,27 @@
-<h2>Create Page</h2>
+@extends('layouts.admin')
+
+
+
+@section('content')
+
+	<h1>Create Users</h1>
+
+		{!! Form::open(['method'=>'POST','action'=>'AdminUsersController@store']) !!}
+	
+			<div class="form-group">	
+				{!! Form::label('name', 'Name:') !!}
+				{!! Form::text('name', null, ['class'=>'form-control']) !!}
+			</div>
+	
+	
+			<div class="form-group">
+			{!! Form::submit('Create User', ['class'=>'btn btn-primary']) !!}
+			</div>
+		
+	
+		{!! Form::close() !!}	
+	
+	 
+		
+
+@endsection
